4a945188427de70d2358bf54ee17f65ed4ef3ac0
Dmitry Ledentsov <dmlled@yahoo.com>
2020-05-30 14:37:26 +0200

    Urigo/graphql-mesh#301

--- gateway/src/mesh/additional-resolvers.js
+++ gateway/src/mesh/additional-resolvers.js
@@ -1,11 +1,10 @@
-const resolvers = {
+module.exports = {
     Widget: {
-        async gadgets( /*parent*/ { id }, /*args*/ _, { Service2 }) {
-            return await Service2.api.gadgets({
+        gadgets: {
+            selectionSet: `{ id }`,
+            resolve: ( /*parent*/ { id }, /*args*/ _, { Service2 }) => Service2.api.gadgets({
                 parentId: id,
-            });
+            }),
         }
     }
 };
-
-module.exports = { resolvers };
